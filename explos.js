e49db4f651c06e18182f14487778feb8520039c6
fominvic81 <fomin.vic81@gmail.com>
2020-02-20 15:22:20 +0200

    Update explos system

--- explos.js
+++ explos.js
@@ -28,37 +28,84 @@ const createExplos = (x, y, dir, power) => {
         spread: false,
         power: power,
 
+        spreadExplos() {
+            for (let i = 1; i <= this.power; ++i) {
+                let nx = this.x;
+                let ny = this.y - i;
+                let npower = this.power - i;
+                if (!tiles[level.map[nx][ny].tile].explosResist) {
+                    level.map[nx][ny].st -= 1;
+                    npower = 0;
+                    level.addEntity(createExplos(nx, ny, 'up', npower));
+                    break;
+                }
+                level.addEntity(createExplos(nx, ny, 'up', npower));
+            }
+            for (let i = 1; i <= this.power; ++i) {
+                let nx = this.x;
+                let ny = this.y + i;
+                let npower = this.power - i;
+                if (!tiles[level.map[nx][ny].tile].explosResist) {
+                    level.map[nx][ny].st -= 1;
+                    npower = 0;
+                    level.addEntity(createExplos(nx, ny, 'down', npower));
+                    break;
+                }
+                level.addEntity(createExplos(nx, ny, 'down', npower));
+            }
+            for (let i = 1; i <= this.power; ++i) {
+                let nx = this.x - i;
+                let ny = this.y;
+                let npower = this.power - i;
+                if (!tiles[level.map[nx][ny].tile].explosResist) {
+                    level.map[nx][ny].st -= 1;
+                    npower = 0;
+                    level.addEntity(createExplos(nx, ny, 'left', npower));
+                    break;
+                }
+                level.addEntity(createExplos(nx, ny, 'left', npower));
+            }
+            for (let i = 1; i <= this.power; ++i) {
+                let nx = this.x + i;
+                let ny = this.y;
+                let npower = this.power - i;
+                if (!tiles[level.map[nx][ny].tile].explosResist) {
+                    level.map[nx][ny].st -= 1;
+                    npower = 0;
+                    level.addEntity(createExplos(nx, ny, 'right', npower));
+                    break;
+                }
+                level.addEntity(createExplos(nx, ny, 'right', npower));
+            }
+        },
+
         update (dt) {
             this.time += dt;
             this.time = Math.min(this.time, this.life_time); 
 
             
-            this.frame = Math.floor(this.time / (this.life_time / this.frameCount));
+            this.frame = Math.min(Math.floor(this.time / (this.life_time / this.frameCount)), this.frameCount - 1);
 
-            if (!tiles[level.map[this.x][this.y].tile].explosResist) {
-                level.map[this.x][this.y].st -= 1;
-                // level.removeEntity(this.id);
-                this.power = 0;
-                // return;
-            }
+            
 
             if (this.power == 0) this.spread = true;
 
             if (!this.spread) {
                 this.spread = true;
                 if (this.dir == 'center') {
-                    level.addEntity(createExplos(this.x, this.y - 1, 'up', this.power - 1))
-                    level.addEntity(createExplos(this.x, this.y + 1, 'down', this.power - 1))
-                    level.addEntity(createExplos(this.x - 1, this.y, 'left', this.power - 1))
-                    level.addEntity(createExplos(this.x + 1, this.y, 'right', this.power - 1))
-                } else if (this.dir == 'up') {
-                    level.addEntity(createExplos(this.x, this.y - 1, this.dir, this.power - 1))
-                } else if (this.dir == 'down') {
-                    level.addEntity(createExplos(this.x, this.y + 1, this.dir, this.power - 1))
-                } else if (this.dir == 'left') {
-                    level.addEntity(createExplos(this.x - 1, this.y, this.dir, this.power - 1))
-                } else if (this.dir == 'right') {
-                    level.addEntity(createExplos(this.x + 1, this.y, this.dir, this.power - 1))
+                    // level.addEntity(createExplos(this.x, this.y - 1, 'up', this.power - 1))
+                    // level.addEntity(createExplos(this.x, this.y + 1, 'down', this.power - 1))
+                    // level.addEntity(createExplos(this.x - 1, this.y, 'left', this.power - 1))
+                    // level.addEntity(createExplos(this.x + 1, this.y, 'right', this.power - 1))
+                    this.spreadExplos();
+                // } else if (this.dir == 'up') {
+                    // level.addEntity(createExplos(this.x, this.y - 1, this.dir, this.power - 1))
+                // } else if (this.dir == 'down') {
+                    // level.addEntity(createExplos(this.x, this.y + 1, this.dir, this.power - 1))
+                // } else if (this.dir == 'left') {
+                    // level.addEntity(createExplos(this.x - 1, this.y, this.dir, this.power - 1))
+                // } else if (this.dir == 'right') {
+                    // level.addEntity(createExplos(this.x + 1, this.y, this.dir, this.power - 1))
                 }
             }
 
