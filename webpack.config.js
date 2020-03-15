0a3a8bbb18dd5b5e65cc780781ed7821087c7d60
Tarik02 <Taras.Fomin@gmail.com>
2020-03-15 11:05:41 +0200

    Fix indent

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const dev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-	devtool: dev ? 'eval-source-map' : false,
-	entry: './src/index.js',
+    devtool: dev ? 'eval-source-map' : false,
+    entry: './src/index.js',
 
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 
-	plugins: [
-		new CopyWebpackPlugin([
-			{ from: './assets', to: 'assets' },
-		]),
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-		}),
-	],
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './assets', to: 'assets' },
+        ]),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
 };
