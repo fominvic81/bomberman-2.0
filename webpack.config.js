74956de488f712d32bc487fdcb694c6e0eef8ba4
Tarik02 <Taras.Fomin@gmail.com>
2020-03-15 10:51:35 +0200

    Add sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const dev = process.env.NODE_ENV === 'development';
+
 module.exports = {
+	devtool: dev ? 'eval-source-map' : false,
 	entry: './src/index.js',
 
 	output: {
